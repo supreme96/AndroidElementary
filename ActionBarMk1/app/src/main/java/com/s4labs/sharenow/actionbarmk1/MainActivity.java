66970755ee1aefc2224c3323c7c99d2df149d52b
supreme96 <sahiltandon1996@gmail.com>
2016-07-09 00:32:52 +0000

    ActionBar Implementation

---
+++ ActionBarMk1/app/src/main/java/com/s4labs/sharenow/actionbarmk1/MainActivity.java
@@ -0,0 +1,52 @@
+package com.s4labs.sharenow.actionbarmk1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch(item.getItemId()) {
+            case R.id.item_1:
+                Toast.makeText(MainActivity.this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+
+
+            case R.id.item_2:
+                Toast.makeText(MainActivity.this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+
+            case R.id.item_3:
+                Toast.makeText(MainActivity.this,"Item 3 selected",Toast.LENGTH_SHORT).show();
+        }
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
